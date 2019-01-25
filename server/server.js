0784a33179baf5651964a7a46990677f5b5ca778
James Page <jcpage02@outlook.com>
2019-01-25 14:47:19 -0700

    fixing hosting server

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const {
 const client = require("twilio")(ACCOUNT_SID, AUTH_TOKEN);
 const stripe = require("stripe")(REACT_APP_STRIPE_SECRET);
 
-app.use( express.static( `${__dirname}/../build` ) )
+app.use(express.static(`${__dirname}/../build`));
 
 app.use(express.json());
 app.use(
